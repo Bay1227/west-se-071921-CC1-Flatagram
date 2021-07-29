b2b2d5d7e802adcaef3c05720d9ddec4cc15e903
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-29 10:57:01 -0700

    Completed Phase 1 Challange

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// write your code here
+
+const imgUrl = 'http://localhost:3000/images/1'
+fetch('http://localhost:3000/images/1')
+.then (resp => resp.json())
+.then((data) => placeImg(data));
+function placeImg (data){
+    document.getElementById("fg-image").src= data.image
+    
+}
+
+
+const likeBtn = document.querySelector('.like-button')
+const spn = document.querySelector('.likes')
+likeBtn.addEventListener('click', () => {
+    spn.innerHTML = '1 like'
+})
+
+const commentForm = document.querySelector('.comment-form')
+commentForm.addEventListener('submit', addComment)
+
+function addComment(e) {
+    e.preventDefault();
+    const commentText = e.target.comment.value
+    const commentList = document.querySelector('.comments')
+
+    const newComm = document.createElement('li')
+    newComm.innerHTML = commentText;
+    commentList.append(newComm);
+}
